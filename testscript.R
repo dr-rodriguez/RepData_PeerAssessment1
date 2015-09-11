f17389bf3840e7f9e450145774a0f4ce6750c016
David Rodriguez <strakul@gmail.com>
2015-09-11 16:33:47 -0300

    done with part 2

--- testscript.R
+++ testscript.R
@@ -1,20 +1,19 @@
 # Script to test out things before adding to the R Markdown file
 
-
+# ========================
 activity <- read.csv('activity.csv', stringsAsFactors=T)
 library(dplyr)
 
+# ========================
 sumdata <- 
     activity %>%
     group_by(date) %>%
     summarize(sum=sum(steps, na.rm=T))
     
 
-mean(sumdata$sum)
-median(sumdata$sum)
-
 library(ggplot2)
 
+# Change to log scale?
 p <- ggplot(sumdata, aes(date, sum)) + 
     geom_bar(fill='darkgreen', col='white', stat="identity") +
     labs(x='Date', y='Total Number of Steps') + 
@@ -24,4 +23,22 @@ p <- ggplot(sumdata, aes(date, sum)) +
 print(p)
 #ggsave('figures/plot1.png')
 
+# Numeric output required in step
+mean(sumdata$sum)
+median(sumdata$sum)
 
+# ========================
+interval5 <-
+    activity %>%
+    mutate(interval=as.factor(interval)) %>%
+    group_by(interval) %>%
+    summarize(mean=mean(steps, na.rm=T))
+# idea: group by interval and take the average of the step, 
+# then plot interval and step average
+
+p <- ggplot(interval5, aes(interval, mean)) +
+    geom_line(col='blue') + 
+    labs(x='Time Interval', y='Average Number of Steps') + 
+    theme_bw()
+
+print(p)
