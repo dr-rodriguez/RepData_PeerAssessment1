f22d27f9308547d1d8db1bb61fb066dd07bdfcfe
David Rodriguez <strakul@gmail.com>
2015-09-11 11:42:09 -0300

    first step done

--- testscript.R
+++ testscript.R
@@ -1,3 +1,27 @@
 # Script to test out things before adding to the R Markdown file
 
-dat <- read.csv('activity.csv')
+
+activity <- read.csv('activity.csv', stringsAsFactors=T)
+library(dplyr)
+
+sumdata <- 
+    activity %>%
+    group_by(date) %>%
+    summarize(sum=sum(steps, na.rm=T))
+    
+
+mean(sumdata$sum)
+median(sumdata$sum)
+
+library(ggplot2)
+
+p <- ggplot(sumdata, aes(date, sum)) + 
+    geom_bar(fill='darkgreen', col='white', stat="identity") +
+    labs(x='Date', y='Total Number of Steps') + 
+    theme_bw() + 
+    theme(axis.text.x = element_text(angle=90))
+
+print(p)
+#ggsave('figures/plot1.png')
+
+
