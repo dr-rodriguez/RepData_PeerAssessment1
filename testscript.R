837d0b89844164057143db96ff90777032b074a7
David Rodriguez <strakul@gmail.com>
2015-09-11 18:42:49 -0300

    corrections to part 3

--- testscript.R
+++ testscript.R
@@ -13,7 +13,6 @@ sumdata <-
 
 library(ggplot2)
 
-# Change to log scale?
 p <- ggplot(sumdata, aes(date, sum)) + 
     geom_bar(fill='darkgreen', col='white', stat="identity") +
     labs(x='Date', y='Total Number of Steps') + 
@@ -58,7 +57,8 @@ activity %>%
     filter(is.na(steps)) %>%
     summarize(countNA=n())
 
-# Replace missing values
+# Replace missing values (option 1)
+# Use average value from intervals
 interval5 <-
     activity %>%
     mutate(interval=as.factor(interval)) %>%
@@ -71,26 +71,71 @@ for(i in seq_along(newactivity$steps)) {
     if(is.na(newactivity$steps[i])) {
         indexarray <- interval5$interval == newactivity$interval[i]
         newactivity$steps[i] <- interval5$mean[indexarray]
-        print(newactivity$steps[i])
-        print(newactivity$interval[i])
-        print(interval5$mean[indexarray])
+        #print(newactivity$steps[i])
+        #print(newactivity$interval[i])
+        #print(interval5$mean[indexarray])
     }
 }
 
-sumdata <- 
+sumdata2 <- 
     newactivity %>%
     group_by(date) %>%
     summarize(sum=sum(steps))
 
-p <- ggplot(sumdata, aes(date, sum)) + 
-    geom_bar(fill='darkgreen', col='white', stat="identity") +
+ggplot(sumdata2, aes(date, sum)) + 
+    geom_bar(fill='darkred', col='white', stat="identity") +
     labs(x='Date', y='Total Number of Steps') + 
     theme_bw() + 
     theme(axis.text.x = element_text(angle=90))
 
-print(p)
-#ggsave('figures/plot1.png')
+# Numeric output required in step
+mean(sumdata2$sum)
+median(sumdata2$sum)
+
+# Some comparisons
+activity %>% filter(date=='2012-11-01')
+newactivity %>% filter(date=='2012-11-01')
+sumdata2 %>% filter(date=='2012-11-01')
+# Problem: because of averaging by interval, each day with NA has same values (10766.19)
+
+# Replace missing values (option 2)
+# Interpolate between days
+sumdata <- 
+    activity %>%
+    group_by(date) %>%
+    summarize(sum=sum(steps))
+# Values with 0 had NA values for the whole day
+
+# Loop through and interpolate around 0 values
+sumdata2 <- sumdata
+len <- nrow(sumdata2)
+for(i in seq_along(sumdata2$sum)) {
+    # Select 0 values
+    if(sumdata2$sum[i] == 0) {
+        print(i)
+        # First element
+        if(i==1) {
+            sumdata2$sum[i] <- sumdata2$sum[i+1]/2
+            next
+        }
+        # Last element
+        if(i==len) {
+            sumdata2$sum[i] <- sumdata2$sum[i-1]/2
+            next
+        }
+        # Normal element
+        print(sumdata2$sum[i])
+        avg <- (sumdata2$sum[i-1] + sumdata2$sum[i+1])/2
+        sumdata2$sum[i] <- avg
+    }
+}
+
+ggplot(sumdata2, aes(date, sum)) + 
+    geom_bar(fill='darkred', col='white', stat="identity") +
+    labs(x='Date', y='Total Number of Steps') + 
+    theme_bw() + 
+    theme(axis.text.x = element_text(angle=90))
 
 # Numeric output required in step
-mean(sumdata$sum)
-median(sumdata$sum)
+mean(sumdata2$sum)
+median(sumdata2$sum)
